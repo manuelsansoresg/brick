8120a5f36c70399419b852cb405bb71585f5e8b0
kinggasper <gem_88@hotmail.com>
2019-12-05 09:50:44 -0600

    cambios
    cambios

--- resources/views/pedido/index.blade.php
+++ resources/views/pedido/index.blade.php
@@ -7,25 +7,28 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0 text-dark">PEDIDOS</h1>
+                    <h1 class="m-0 text-dark">Pedido</h1>
                 </div><!-- /.col -->
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">MENU</a></li>
-                        <li class="breadcrumb-item active">PEDIDOS</li>
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">Pedidos</li>
                     </ol>
                 </div><!-- /.col -->
             </div><!-- /.row -->
+
             <div class="row">
                 <div class="col-12">
                     <div class="card">
-                        <div class="card-header"> .:: LISTA DE PEDIDOS ::.</div>
-                        <div class="card-body">
-                            <div class="row justify-content-end mt-3">
-                                <div class="col-2">
-                                    <a href="/admin/pedido/create" class="btn btn-block btn-primary btn-sm"><i class="far fa-file"></i> Nuevo</a>
+                        <div class="card-header">
+                            <div class="d-flex align-items-center">
+                              <h3 class="mr-auto">LISTA DE PEDIDOS</h3>
+                                <div>
+                                    <a href="/admin/pedido/create" class="btn btn-block btn-primary btn-sm"><i class="far fa-file"></i> AGREGAR</a>
                                 </div>
                             </div>
+                        </div>
+                        <div class="card-body">                            
                             <div class="row mt-3">
                                 <div class="col-12">
                                     <table class="table-default table table-bordered table-hover dataTable" role="grid" aria-describedby="example2_info">
