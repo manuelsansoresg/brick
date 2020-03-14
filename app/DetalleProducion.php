6e3da7adf47b7eee975d24c4c4ccc7a315f91d19
kinggasper <gem_88@hotmail.com>
2020-03-14 12:39:16 -0600

    Coreige esto
    cambios corregi la snetencia

--- app/DetalleProducion.php
+++ app/DetalleProducion.php
@@ -8,10 +8,22 @@ class DetalleProducion extends Model
 {
     protected $table      = 'detalleproducion';
     protected $primaryKey = 'IdProducion';
+    protected $fillable = ['IdProducion', 'IdProducto', 'Cantidad', 'Precio', 'Importe', 'Observaciones', 'Estatus', 'clasificacion','descripcion','total'];
 
     public static function getByProduccion($produccion_id)
     {
-        return DetalleProducion::where('IdProducion',$produccion_id)->get();               
+        return DetalleProducion::where('IdProducion',$produccion_id)->get();  
+        
+        /*$detalle = DetalleProducion::select('detalleproducion.IdProducion','articulo.descripcion','detalleproducion.IdProducto','detalleproducion.Cantidad',
+                                'detalleproducion.clasificacion','detalleproducion.Estatus','detalleproducion.Observaciones','detalleproducion.Precio',
+        'detalleproducion.Importe','(SELECT SUM(detalleproducionavance.Cantidad)
+                FROM detalleproducionavance
+                WHERE detalleproducionavance.IdProducion = detalleproducion.IdProducion
+                AND detalleproducionavance.IdProducto= detalleproducion.IdProducto) AS total)')
+            ->join('articulo', 'articulo.Id', '=', 'detalleproducion.IdProducto', 'INNER')            
+            ->where('detalleproducion.IdProducion', $produccion_id)
+            ->get();
+        return $detalle;*/
     }
 
     public static function getByIds($IdProducion, $IdProducto)
